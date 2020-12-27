f2bf2bd77ff7dc9f67f16ffea1f02a47f74f708b
wuqianye <natsumi3613@gmail.com>
2020-12-26 22:10:07 -0500

    10 completed

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,11 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+    var count = 0;
+    nums.forEach(element => {
+        if (element >= start && element <= end) {
+            count++;
+        }
+    });
+    return count;
 }
 
 // Do not edit this line;

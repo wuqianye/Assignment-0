2ff48573ef333c2c3a9c704bca74813aa6e29c38
wuqianye <natsumi3613@gmail.com>
2020-12-27 00:01:50 -0500

    18 completed

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,15 @@
 function pairSum(nums, target) {
-  // Insert code here;
+    if (nums.length < 2) {
+        throw "not enough number";
+    }
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 // Do not edit this line;

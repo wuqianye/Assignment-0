9d5972aa362575d4055d2a62dba3bb0bd3651d66
wuqianye <natsumi3613@gmail.com>
2020-12-26 22:59:10 -0500

    13 completed

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
-  // Insert code here;
+    var length = word.length -1;
+    var halfLength = Math.ceil(length / 2);
+    for (var i = 0; i < halfLength; i++) {
+        if (word[i] != word[length - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 // Do not edit this line;

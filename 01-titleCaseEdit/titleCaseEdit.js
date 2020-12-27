574e9a9326e29ad134e228c840a6e913e5f41289
wuqianye <natsumi3613@gmail.com>
2020-12-26 21:28:55 -0500

    01 completed

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,13 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+    var words = title.split(" ");
+    title = "";
+    words.forEach(element => {
+        var characters = element.split("");
+        characters[0] = characters[0].toUpperCase();
+        element= characters.join("");
+        title += element + " ";
+    });
+    return title.trim();
 }
 
 // Do not edit this line;

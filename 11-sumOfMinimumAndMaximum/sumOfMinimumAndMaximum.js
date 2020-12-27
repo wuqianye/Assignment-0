22978df91b6c17c2d8ec097e69d76d7ddd14673b
wuqianye <natsumi3613@gmail.com>
2020-12-26 22:30:31 -0500

    11 completed

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,8 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+    var min = Math.min(...nums);
+    var max = Math.max(...nums);
+    var sum = min + max;
+    return sum;
 }
 
 // Do not edit this line;

52e372dea609a9ed9340ce721b65f021c7f2b66a
wuqianye <natsumi3613@gmail.com>
2020-12-26 22:45:26 -0500

    12 completed

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,19 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+    var arr = [];
+    var num = start;
+    for (var i = 0; i <= end-start; i++) {
+        if (num % 3 == 0 && num % 5 == 0) {
+            arr[i] = "FizzBuzz";
+        } else if (num % 3 == 0) {
+            arr[i] = "Fizz";
+        } else if (num % 5 == 0) {
+            arr[i] = "Buzz";
+        } else {
+            arr[i] = num;
+        }
+        num++;
+    }
+    return arr;
 }
 
 // Do not edit this line;

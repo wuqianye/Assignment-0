2f4c793b3f65e22642598102b7e0aa2af485c5f7
wuqianye <natsumi3613@gmail.com>
2020-12-26 23:51:53 -0500

    17 completed

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,13 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+    var mySet = new Set();
+    for (let set of args) {
+        for (let num of set) {
+            if (!(mySet.has(num))) {
+                mySet.add(num);
+            }
+        }
+    }
+    return mySet;
 }
 
 // Do not edit this line;

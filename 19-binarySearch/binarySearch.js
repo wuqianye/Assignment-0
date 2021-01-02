7520d2cf870afa2041a8effc4744d63d8b6de34b
wuqianye <natsumi3613@gmail.com>
2021-01-02 18:34:08 -0500

    19 completed

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -1,11 +1,31 @@
 class MySolution {
-  constructor() {
-    this.flag = false; // this is optional to use;
-  }
+    constructor() {
+        this.flag = false; // this is optional to use;
+    }
 
-  binarySearch(nums, target) {
-    // Insert code here;
-  }
+    binarySearch(nums, target) {
+        var mid = Math.floor((nums.length) / 2);
+        if (nums[mid] == target) {
+            return true;
+        } else if (nums[mid] != target && nums.length == 1) {
+            return false;
+        } else if (nums[mid] > target) {
+            var mNums = [];
+            for (var i = 0; i < mid; i++) {
+                mNums.push(nums[i]);
+            }
+            return this.binarySearch(mNums, target);
+        } else if (nums[mid] < target) {
+            if (nums.length - 1 == mid) {
+                return false;
+            }
+            var mNums = [];
+            for (var i = mid + 1; i < nums.length; i++) {
+                mNums.push(nums[i]);
+            }
+            return this.binarySearch(mNums, target);
+        }
+    }
 }
 
 // Do not edit this line;
